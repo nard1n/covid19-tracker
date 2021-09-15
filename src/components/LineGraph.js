6103a44a4259b37359a25b5fc01e5e79ae6a4b78
Nardin Lachowski <nardin@live.com>
2021-09-14 22:06:18 -0400

    cleaned up the code and made a few minor corrections such as cdeclaring chartData as let rather than a const

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -47,8 +47,8 @@ const options = {
     },
   };
   
-  const buildChartData = (data, casesType = "recovered") => {
-    const chartData = [];
+  const buildChartData = (data, casesType) => {
+    let chartData = [];
     let lastDataPoint;
 
     for (let date in data.cases) {
@@ -64,20 +64,23 @@ const options = {
     return chartData;
   };
 
-function LineGraph({casesType}) {
+function LineGraph({ casesType = "cases" }) {
   const [data, setData] = useState({});
+
   useEffect(() => {
       const fetchData = async () => {
         await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
-        .then((response) => response.json())
+        .then((response) => {
+          return response.json();
+        })
         .then((data) => {
           let chartData = buildChartData(data, casesType);
           setData(chartData);
         });
-      }
+      };
       fetchData();
     
-  }, []);
+  }, [casesType]);
 
   return (
     <div>
@@ -85,11 +88,13 @@ function LineGraph({casesType}) {
             <Line 
             options={options}    
             data={{
-                    datasets: [{
+                    datasets: [
+                      {
                         backgroundColor: "rgba(204, 16, 52, 0.5)",
                         borderColor: "#CC1034",
                         data: data,
-                    }]
+                    },
+                  ],
                 }} 
             />
       )}
