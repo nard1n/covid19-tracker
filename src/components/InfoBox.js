c04d5c7db084b5337d08249aa7ca96215b5ea1ec
Nardin Lachowski <nardin@live.com>
2021-09-16 17:18:32 -0400

    adding styling to infobox when active

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,9 +1,9 @@
 import { Card, CardContent, Typography } from "@material-ui/core";
 import "./InfoBox.css";
 
-function InfoBox({ title, cases, total, ...props }) {
+function InfoBox({ title, cases, total, active, ...props }) {
     return (
-        <Card className="infoBox" onClick={props.onClick}>
+        <Card className={`infoBox ${active && "infoBox--selected"}`} onClick={props.onClick}>
             <CardContent>
 
                 <Typography classeName='infoBox__title' color="textSecondary">
@@ -11,8 +11,8 @@ function InfoBox({ title, cases, total, ...props }) {
                 </Typography>
 
 
-                <h2 classeName='infoBox__cases'>
-                    {cases}
+                <h2 style={{color: "#485063"}} classeName='infoBox__cases'>
+                    +{cases}
                 </h2>
 
                 <Typography color="textSecondary" classeName='infoBox__total' >
