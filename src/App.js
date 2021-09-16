65216c104411500dde4632e74492a0d21b76f2af
Nardin Lachowski <nardin@live.com>
2021-09-16 14:19:50 -0400

    introduced a new state to keep track of the cases the user whats to view when they click on any of the infoboxes

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
   const [mapZoom, setMapZoom] = useState(3);
   const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
   const [mapCountries, setMapCountries] = useState([]);
+  const [casesType, setCasesType] = useState('cases');
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -90,14 +91,17 @@ function App() {
 
         <div className="app__stats">
           <InfoBox
+           onClick={e => setCasesType('cases')}
            title="Coronavirus Cases"
            cases={prettyPrintStat(countryInfo.todayCases)}
            total={prettyPrintStat(countryInfo.cases)} />
           <InfoBox
+           onClick={e => setCasesType('recovered')}
            title="Recovered"
            cases={prettyPrintStat(countryInfo.todayRecovered)}
            total={prettyPrintStat(countryInfo.recovered)} />
           <InfoBox
+           onClick={e => setCasesType('deaths')}
            title="Deaths"
            cases={prettyPrintStat(countryInfo.todayDeaths)}
            total={prettyPrintStat(countryInfo.deaths)} />
